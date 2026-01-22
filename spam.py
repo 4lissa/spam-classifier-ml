9776d2671287438ada593bc57e8c945221423110
4lissa <alissa.gritti@epitech.eu>
2026-01-22 20:01:29 +0100

    feat: add TF-IDF text vectorization

--- spam.py
+++ spam.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 df = pd.read_csv('spam.csv', encoding='latin-1')
 
@@ -7,8 +9,6 @@ df.columns = ['label', 'message']
 
 print("Number of rows and columns:", df.shape)
 print(df.head())
-
-from sklearn.model_selection import train_test_split
 
 df['label'] = df['label'].map({'ham': 0, 'spam': 1})
 
@@ -19,3 +19,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 print("Training set size:", X_train.shape)
 print("Testing set size:", X_test.shape)
+
+vectorizer = TfidfVectorizer(stop_words='english')
+
+X_train_vectorized = vectorizer.fit_transform(X_train)
+X_test_vectorized = vectorizer.transform(X_test)    
+
+print("X_train_vectorized shape:", X_train_vectorized.shape)
+print("X_test_vectorized shape:", X_test_vectorized.shape)
