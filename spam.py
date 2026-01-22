f2061e473aeff3fad5c14ff0eb71e677b49ad7fe
4lissa <alissa.gritti@epitech.eu>
2026-01-22 19:50:21 +0100

    feat: split dataset into train and test

--- spam.py
+++ spam.py
@@ -7,3 +7,15 @@ df.columns = ['label', 'message']
 
 print("Number of rows and columns:", df.shape)
 print(df.head())
+
+from sklearn.model_selection import train_test_split
+
+df['label'] = df['label'].map({'ham': 0, 'spam': 1})
+
+X = df['message']
+y = df['label']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print("Training set size:", X_train.shape)
+print("Testing set size:", X_test.shape)
